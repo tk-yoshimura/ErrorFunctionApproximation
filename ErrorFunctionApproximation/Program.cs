8729d42ff31aa7daeea4809007c2e15018ff4183
tk-yoshimura <yoshimura8192@gmail.com>
2021-03-22 20:52:02 +0900

    + approximate code

--- ErrorFunctionApproximation/Program.cs
+++ ErrorFunctionApproximation/Program.cs
@@ -1,9 +1,47 @@
-﻿using System;
+﻿using MultiPrecision;
+using System;
 
 namespace ErrorFunctionApproximation {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            for (int z = 2; z <= 10; z++) { 
+                MultiPrecision<Pow2.N4> erfc = ErfcContinuedFraction<Pow2.N4>.Erfc(z, 1);
+            }
+
+            for (int n = 0; n <= 100; n++) {
+                MultiPrecision<Pow2.N4> erfc = ErfcContinuedFraction<Pow2.N4>.Erfc(2, n);
+
+                Console.WriteLine($"{n} : {erfc}");
+            }
+
+            Console.WriteLine(string.Empty);
+
+            for (int n = 0; n <= 100; n++) {
+                MultiPrecision<Pow2.N4> erfc = ErfcContinuedFraction<Pow2.N4>.Erfc(3, n);
+
+                Console.WriteLine($"{n} : {erfc}");
+            }
+
+            Console.WriteLine(string.Empty);
+
+            for (int n = 0; n <= 100; n++) {
+                MultiPrecision<Pow2.N4> erfc = ErfcContinuedFraction<Pow2.N4>.Erfc(4, n);
+
+                Console.WriteLine($"{n} : {erfc}");
+            }
+
+            Console.WriteLine(string.Empty);
+
+            for (int n = 0; n <= 100; n++) {
+                MultiPrecision<Pow2.N4> erfc = ErfcContinuedFraction<Pow2.N4>.Erfc(8, n);
+
+                Console.WriteLine($"{n} : {erfc}");
+            }
+
+            Console.WriteLine(string.Empty);
+
+            Console.WriteLine("END");
+            Console.Read();
         }
     }
 }
